2d008f9e68e96f6f5f3cb9c447036522f0d32739
Pogryziony <Pogryziony98@interia.pl>
2023-06-10 11:09:32 +0200

    Add second exercise

---
+++ excersise_two/check_for_palindrome.py
@@ -0,0 +1,18 @@
+def is_palindrome():
+    text = input("Enter text: ")
+    text = text.strip()
+    text = text.lower()
+    reversed_text = text[::-1]
+    if text == reversed_text:
+        print("Text you have entered is a palindrome")
+        print("Text: " + text)
+        print("Reversed text :" + reversed_text)
+        return True
+    else:
+        print("Text is not a palindrome")
+        print("Text: " + text)
+        print("Reversed text :" + reversed_text)
+        return False
+
+class CheckForPalindrome:
+    pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from exercise_one.sum_of_array import sum_array
+from excersise_two.check_for_palindrome import is_palindrome
 
-sum_array()
+is_palindrome()
